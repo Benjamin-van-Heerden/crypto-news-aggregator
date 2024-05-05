b0daf43be71b499f1e20888f1031e6ec96d118d0
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2024-05-05 21:44:10 +0200

    check

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("hello world")
-a = 5
-print(f"a + 2 is {a + 2}")
+def check_a_thing():
+  count = 0 
+  for i in range(10):
+    count += 1
+  print(count)
+
+
+if __name__ == "__main__":
+  check_a_thing()
+  print("I am here")
+   
